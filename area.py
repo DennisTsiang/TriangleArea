d4526494a5064ee213d7863b783790aa2e97177a
Dennis <dennis.tsiang15@imperial.ac.uk>
2017-06-15 11:44:49 +0100

    Added area.py

---
+++ area.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+def calculateArea(base, height):
+    return (base*height)/2
+
+
+if __name__ == "__main__":
+     script, base_str, height_str = argv
+     base = int(base_str)
+     height = int(height_str)
+     print("You enter base: %d height: %d" % (base, height))
+     print("Area: %d" % (calculateArea(base, height)))
